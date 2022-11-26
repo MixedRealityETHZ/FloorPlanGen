1c80074ceb98b00038d8e632d25db4579960ad0b
ThomasPoyet <thomas.poyet99@gmail.com>
2022-11-26 15:46:58 +0100

    adding tracking status checking

--- user_interface/Assets/Scripts/TrackingHub.cs
+++ user_interface/Assets/Scripts/TrackingHub.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using UnityEngine;
+using Vuforia;
 
 public class TrackingHub : MonoBehaviour
 {
     private Dictionary<int, GameObject> UIs = new Dictionary<int, GameObject>();
+    private Dictionary<int, GameObject> modelTargets = new Dictionary<int, GameObject>();
+    private Dictionary<int, bool> previousTrackingStatus = new Dictionary<int, bool>();
 
     // Start is called before the first frame update
     void Start()
@@ -17,22 +20,45 @@ public class TrackingHub : MonoBehaviour
         {
             UIs.Add(gameObject.GetComponent<Node>().id, gameObject);
         }
-        UnityEngine.Debug.Log(UIs.Count);
+
+        foreach (Transform childT in transform)
+        {
+            GameObject child = childT.gameObject;
+            int childID = child.GetComponent<ModelTargetID>().id;
+            modelTargets.Add(childID, child);
+            previousTrackingStatus.Add(childID, false);
+        }
+
+        System.Diagnostics.Debug.Assert(UIs.Count == modelTargets.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (Transform childT in transform)
+        foreach (KeyValuePair<int, GameObject> item in modelTargets)
         {
-            GameObject child = childT.gameObject;
-            int childID = child.GetComponent<ModelTargetID>().id;
+            int modelTargetID = item.Key;
+            GameObject modelTarget = item.Value;
+            Vuforia.ModelTargetBehaviour modelTargetBehaviour = modelTarget.GetComponent<ModelTargetBehaviour>();
             GameObject UI;
-            UIs.TryGetValue(childID, out UI);
+            UIs.TryGetValue(modelTargetID, out UI);
+
+            //Check tracking status
+            bool previousStatus;
+            previousTrackingStatus.TryGetValue(modelTargetID, out previousStatus);
+            bool currentTrackingStatus = (modelTargetBehaviour.TargetStatus.Status == Vuforia.Status.TRACKED);
+            if (previousStatus != currentTrackingStatus)
+            {
+                UnityEngine.Debug.Log("ModelTarget: " + modelTargetID.ToString() + " tracking status is " + currentTrackingStatus.ToString());
+                //ToDo: Prevenir UI
+                previousTrackingStatus[modelTargetID] = currentTrackingStatus;
+            }
+
+            //Update UI orientation and position
             if (UI != null)
             {
                 //Set position of the UI according to the model target
-                Vector3 trackedPosition = child.transform.position;
+                Vector3 trackedPosition = modelTarget.transform.position;
                 UI.transform.position = new Vector3(trackedPosition.x, trackedPosition.y + 0.02f, trackedPosition.z); //ToDo: Coller le y sur une surface
 
                 //Set the orientation such that face the user i.e. the camera
